f8494fe12f3b39f976b06a05de8783bcdac57738
Max Strokov <maxorrs@gmail.com>
2020-07-28 14:18:18 +0300

    Рендерит компоненты сайта

--- src/main.js
+++ src/main.js
@@ -328,37 +328,36 @@ const render = (container, place, template) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const pageBody = document.querySelector('.page-body');
-const header = pageBody.querySelector('.page-header');
-const headerContainer = header.querySelector('.page-header__container');
-const tripMainContainer = header.querySelector('.trip-main');
-const tripControls = tripMainContainer.querySelector('.trip-controls');
-const firstTitleTripControls = tripControls.querySelector('h2');
+const pageBody = document.querySelector(`.page-body`);
+const header = pageBody.querySelector(`.page-header`);
+const tripMainContainer = header.querySelector(`.trip-main`);
+const tripControls = tripMainContainer.querySelector(`.trip-controls`);
+const firstTitleTripControls = tripControls.querySelector(`h2`);
 
-render(tripMainContainer, 'afterBegin', createTripInfoTemplate());
-render(firstTitleTripControls, 'afterEnd', createTripTabsTemplate());
-render(tripControls, 'beforeEnd', createTripFiltersTemplate());
+render(tripMainContainer, `afterBegin`, createTripInfoTemplate());
+render(firstTitleTripControls, `afterEnd`, createTripTabsTemplate());
+render(tripControls, `beforeEnd`, createTripFiltersTemplate());
 
-const main = pageBody.querySelector('.page-main');
-const tripEventsContainer = main.querySelector('.trip-events');
+const main = pageBody.querySelector(`.page-main`);
+const tripEventsContainer = main.querySelector(`.trip-events`);
 
-render(tripEventsContainer, 'beforeEnd', createTripSortTemplate());
+render(tripEventsContainer, `beforeEnd`, createTripSortTemplate());
 
-render(tripEventsContainer, 'beforeEnd', createEventTemplate());
+render(tripEventsContainer, `beforeEnd`, createEventTemplate());
 
-render(tripEventsContainer, 'beforeEnd', createDaysContainerTemplate());
+render(tripEventsContainer, `beforeEnd`, createDaysContainerTemplate());
 
-const daysContainer = tripEventsContainer.querySelector('.trip-days');
+const daysContainer = tripEventsContainer.querySelector(`.trip-days`);
 
 if (daysContainer) {
-  render(daysContainer, 'beforeEnd', createOneDayTemplate());
+  render(daysContainer, `beforeEnd`, createOneDayTemplate());
 }
 
-const tripEventsList = tripEventsContainer.querySelector('.trip-events__list');
+const tripEventsList = tripEventsContainer.querySelector(`.trip-events__list`);
 
 if (tripEventsList) {
   for (let i = 0; i < COUNT_WAYPOINTS; i++) {
-    render(tripEventsList, 'beforeEnd', createWaypointTemplate());
+    render(tripEventsList, `beforeEnd`, createWaypointTemplate());
   }
 }
 
